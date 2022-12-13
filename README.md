125f7d5a11ab2b313317d904e071ff406112e8f1
Teja Dandu <teja.dandu484@gmail.com>
2022-12-13 11:03:16 +0530

    Setup basic dotenv

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# Welcome to Flights Service
+
+## Project Setup
+- clone the project on your local
+- Execute `npm install` on the same path as of your root directory of the
+downloaded project
+- Create a `.env` file in the root directory and add the following envronment
 /
     - src/
         index.js // server
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {PORT} = require('./config/serverConfig');
+
+
+const setupAndStartServer = async () => {
+    // Create the express object
+
+    const app = express();
+
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+
+    });
+}
+
+setupAndStartServer();
